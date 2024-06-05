a4a89ba1b339099088440041333bf1024b0addfc
jpeetla <ynv7jk@virginia.edu>
2024-06-05 15:14:56 -0400

    started implementation of signup

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+import pyrebase
 app = Flask(__name__)
 
+config = {
+    'apiKey': "AIzaSyBASkRSuh9GQdz95cPf2DHGtiI3tDCifEY",
+    'authDomain': "wahootunes.firebaseapp.com",
+    'projectId': "wahootunes",
+    'storageBucket': "wahootunes.appspot.com",
+    'messagingSenderId': "701311223380",
+    'appId': "1:701311223380:web:d8203363409f77dbeccfc1",
+    'measurementId': "G-HRS3VHRX00",
+    'databaseURL': ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+app.secret_key = 'secret'
 @app.route("/")
 def hello():
-  return render_template('welcome.html')
+    if('user' in session):
+        return render_template("home.html")
+    else:
+        return render_template('welcome.html')
 
 @app.route("/login")
 def goToLogin():
@@ -15,14 +34,19 @@ def goToSignUp():
 
 @app.route('/home', methods=['POST'])
 def goHome():
+    if request.method == "POST":
+        fullname = request.form.get("fullname")
+        uvaID = request.form.get("uvaID")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            session["user"] = email
+        except:
+            return "Failed to Sign Up"
+
     return render_template('home.html')
-
-def logIn():
-    return "hi"
-
-def signUp():
-    return "hi"
-
 
 if __name__ == "__main__":
   app.run()
