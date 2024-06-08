5fb1334f37c4079122768357d67e884962499d9e
asanjideh <ariana.sanjideh@student.unimelb.edu.au>
2024-06-08 18:56:00 -0400

    Added rating page

--- app.py
+++ app.py
@@ -61,6 +61,33 @@ def goToSignUp():
 def goHome():
     return render_template('home.html')
 
+def get_songs():
+    conn = sqlite3.connect('music.sqlite3')
+    cursor = conn.cursor()
+    cursor.execute("SELECT SongID, SongTitle FROM Song")
+    songs = cursor.fetchall()
+    conn.close()
+    return songs
+
+@app.route('/rate', methods=['GET', 'POST'])
+def rate():
+    if request.method == 'POST':
+        song_id = request.form['song']
+        rating = request.form['rating']
+        review = request.form['review']
+        
+        conn = sqlite3.connect('music.sqlite3')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Rating (RatingValue, Review, SongID, UserID) VALUES (?, ?, ?, ?)", (rating, review, song_id, 1))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('rate'))
+    
+    songs = get_songs()
+    return render_template('rating.html', songs=songs)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
